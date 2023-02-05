fb839d8cf77693150264125ddbd9c21700d2d4d4
Zaid Marrie <zaidmarrie27@gmail.com>
2023-02-05 16:27:55 +0200

    feat: add user customized quiz presets

--- src/components/OpeningScreen.jsx
+++ src/components/OpeningScreen.jsx
@@ -1,19 +1,108 @@
-import blobBottomLeft from "../assets/blob-bottom.png";
-import blobTopRight from "../assets/blob-top.png";
+import { useState } from "react";
+import { usePresets } from "@/context/PresetsContext";
+import styles from "@/styles/OpeningScreen.module.css";
 
 function OpeningScreen({ startGame }) {
-	return (
-		<div className="opening-screen centre">
-			<h1 className="title">Quizzical</h1>
-			<p className="desc">
-				A muliple-choice quiz of 5 random general knowledge questions pulled from the Open Trivia DB API.
-			</p>
-			<button className="btn" onClick={startGame}>
-				Start quiz
-			</button>
+	const { presets, setPresets } = usePresets();
+	const [formData, setFormData] = useState(presets);
 
-			<img src={blobTopRight} alt="A circular blob" className="blob blob--top-right" />
-			<img src={blobBottomLeft} alt="A circular blob" className="blob blob--bottom-left" />
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		setPresets(formData);
+	};
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+
+		setFormData((prevPresets) => ({ ...prevPresets, [name]: value }));
+	};
+
+	return (
+		<div className={styles.screen}>
+			<h1>Quizzical</h1>
+
+			<p>
+				Welcome to my trivia quiz app. This is a multiple choice based game that
+				tests your knowledge on various categories by pulling in questions from
+				the Open Trivia DB API. You are given a number of questions to answer
+				within the allocated time frame of 30 seconds. Try your best to get as
+				much questions correct by answering each question as accurately as
+				possible.
+			</p>
+
+			<form className={styles.form} onSubmit={handleSubmit}>
+				<div className={styles.form__group}>
+					<label htmlFor="questionCount">Number of questions</label>
+					<input
+						type="number"
+						name="questionCount"
+						id="questionCount"
+						min="5"
+						max="30"
+						className={styles.form__input}
+						value={formData.questionCount}
+						onChange={handleChange}
+					/>
+				</div>
+
+				<div className={styles.form__group}>
+					<label htmlFor="difficulty">Select Difficulty</label>
+					<select
+						name="difficulty"
+						id="difficulty"
+						className={styles.form__input}
+						value={formData.difficulty}
+						onChange={handleChange}
+					>
+						<option value="any">Any Difficulty</option>
+						<option value="easy">Easy</option>
+						<option value="medium">Medium</option>
+						<option value="hard">Hard</option>
+					</select>
+				</div>
+
+				<div className={styles.form__group}>
+					<label htmlFor="category">Select a category</label>
+					<select
+						name="category"
+						id="category"
+						className={styles.form__input}
+						value={formData.category}
+						onChange={handleChange}
+					>
+						<option value="any">Any Category</option>
+						<option value="9">General Knowledge</option>
+						<option value="10">Entertainment: Books</option>
+						<option value="11">Entertainment: Film</option>
+						<option value="12">Entertainment: Music</option>
+						<option value="13">Entertainment: Musical & Theatres</option>
+						<option value="14">Entertainment: Television</option>
+						<option value="15">Entertainment: Video Games</option>
+						<option value="16">Entertainment: Board Games</option>
+						<option value="17">Science & Nature</option>
+						<option value="18">Science: Computers</option>
+						<option value="19">Science: Mathematics</option>
+						<option value="20">Mythology</option>
+						<option value="21">Sports</option>
+						<option value="22">Geography</option>
+						<option value="23">History</option>
+						<option value="24">Politics</option>
+						<option value="25">Art</option>
+						<option value="26">Celebrities</option>
+						<option value="27">Animals</option>
+						<option value="28">Vehicles</option>
+						<option value="29">Entertainment: Comics</option>
+						<option value="30">Science: Gadgets</option>
+						<option value="31">Entertainment: Japanese Anime & Manga</option>
+						<option value="32">Entertainment: Cartoon & Animations</option>
+					</select>
+				</div>
+
+				<button type="submit" className={styles.form__btn} onClick={startGame}>
+					Start Quiz
+				</button>
+			</form>
 		</div>
 	);
 }
